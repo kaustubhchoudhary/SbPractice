b7ca6f973e5fb7e2d3b09c2d1d0fa5cdf3a5df6d
kaustubhchoudhary <kaustubhchoudhary@yahoo.com>
2025-08-20 02:24:25 +0530

    Implemented full Spring Security with role-based access using inMemory and Postgres

--- src/main/java/sb/practice/repositories/UserRepository.java
+++ src/main/java/sb/practice/repositories/UserRepository.java
@@ -1,15 +1,22 @@
 package sb.practice.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import sb.practice.dto.UserLoginDTO;
 import sb.practice.entities.User;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    // SELECT * FROM users WHERE username = ?
-
-    // Criteria API
     boolean existsByUsername(String username);
     boolean existsByEmailId(String emailId);
+
+    @Query("SELECT new sb.practice.dto.UserLoginDTO(u.username, u.passwordSalt, u.passwordHash, r.roleName) FROM User u JOIN u.role r WHERE u.username = :username")
+    Optional<UserLoginDTO> findLoginDetailsByUsername(@Param("username") String username);
+
+
 }
